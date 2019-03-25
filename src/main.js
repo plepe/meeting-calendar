f692ba2aa0eeeef5a06939df354826adbe09ca27
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2019-03-25 10:25:46 +0100

    Improve format

--- src/main.js
+++ src/main.js
@@ -11,12 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
   let lastDate = events[events.length - 1].end
 
   var calendar = new Calendar(calendarEl, {
+    locale: 'en-gb',
+    header: false,
     plugins: [ timeGridPlugin ],
     defaultView: 'timeGridCustom',
     minTime: '08:00:00',
     maxTime: '21:00:00',
     events,
     defaultDate: firstDate,
+    height: 'parent',
+    allDaySlot: false,
+    slotLabelFormat: { hour: 'numeric', minute: '2-digit', omitZeroMinute: false, meridiem: false },
     views: {
       timeGridCustom: {
         type: 'timeGrid',
