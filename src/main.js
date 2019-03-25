00ba0fccbc08c7056c7f1995fb0c7ff5326d09d4
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2019-03-25 09:39:34 +0100

    Show events from events.json file

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 const Calendar = require('@fullcalendar/core').Calendar
 const timeGridPlugin = require('@fullcalendar/timegrid').default
 
+let events = require('../events.json')
+
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar')
 
   var calendar = new Calendar(calendarEl, {
     plugins: [ timeGridPlugin ],
     defaultView: 'timeGridWeek',
-    hiddenDays: [ 2, 4 ],
     minTime: '08:00:00',
     maxTime: '21:00:00',
-    firstDay: 1
+    firstDay: 1,
+    events,
+    defaultDate: events[0].start
   })
 
   calendar.render()
